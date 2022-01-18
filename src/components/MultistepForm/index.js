54ae4e0605fb799cfdba11ec75f326a9de281ade
Krisztian Ivan <krisztian.ivan@gmail.com>
2022-01-18 16:09:30 +0000

    added boilerplate multistep and buttons

--- src/components/MultistepForm/index.js
+++ src/components/MultistepForm/index.js
@@ -26,6 +26,36 @@ const MultistepForm = () => {
     shouldFocusError: true,
   });
 
+  const renderButton = () => {
+    if (formStep === 0) {
+      return (
+        <button type="button" onClick={nextFormStep}>
+          Next
+        </button>
+      );
+    } else if (formStep === 1) {
+      return (
+        <div>
+          <button type="button" onClick={lastFormStep}>
+            Previous
+          </button>
+          <button type="button" onClick={nextFormStep}>
+            Next
+          </button>
+        </div>
+      );
+    } else if (formStep === 2) {
+      return (
+        <div>
+          <button type="button" onClick={lastFormStep}>
+            Previous
+          </button>
+          <button type="submit">Submit</button>
+        </div>
+      );
+    }
+  };
+
   const onSubmit = (data) => console.log(data);
 
   return (
@@ -39,6 +69,35 @@ const MultistepForm = () => {
               type="text"
               {...register("companyName", { required: true })}
             ></input>
+            {renderButton()}
+          </div>
+        )}
+
+        {formStep === 1 && (
+          <div className="form-element">
+            <input
+              className="form-input"
+              placeholder="Company Number"
+              type="text"
+              {...register("companyNumber", { required: true })}
+            ></input>
+            {renderButton()}
+          </div>
+        )}
+
+        {formStep === 2 && (
+          <div className="form-element">
+            <input
+              className="form-input"
+              placeholder="Company Email"
+              type="email"
+              {...register("companyEmail", {
+                required: true,
+                pattern:
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              })}
+            ></input>
+            {renderButton()}
           </div>
         )}
       </form>
